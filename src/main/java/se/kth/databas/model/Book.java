31f108e7e4b4bc9dc3d23b7dc21916d884da26bd
Conrad <connevonne@gmail.com>
2023-12-07 15:26:01 +0100

    betyg

--- src/main/java/se/kth/databas/model/Book.java
+++ src/main/java/se/kth/databas/model/Book.java
@@ -15,25 +15,39 @@ public class    Book {
     private String isbn; // should check format
     private String title;
     private Date published;
+    private int grade;
+
     private String storyLine = "";
     private ArrayList<Author> authors = new ArrayList<>();
 
     // TODO:
     // Add authors, as a separate class(!), and corresponding methods, to your implementation
     // as well, i.e. "private ArrayList<Author> authors;"
-    public Book(int bookId, String isbn, String title, Date published) {
+    public Book(int bookId, String isbn, String title, Date published, int grade) {
         this.bookId = bookId;
         this.isbn = isbn;
         this.title = title;
         this.published = published;
+        this.grade = grade;
     }
-    
+
+
+    // Additional constructor without the grade field...
     public Book(String isbn, String title, Date published) {
-        this(-1, isbn, title, published); 
+        this(-1, isbn, title, published, 0); // Default grade is 0
     }
-    
+    public Book(String isbn, String title, Date published, int grade) {
+        this(-1, isbn, title, published, grade);
+    }
     public int getBookId() { return bookId; }
     public String getIsbn() { return isbn; }
+    public int getGrade() {
+        return grade;
+    }
+    public void setGrade(int grade) {
+        // You can add validation logic here if needed
+        this.grade = grade;
+    }
     public String getTitle() { return title; }
     public Date getPublished() { return published; }
     public String getStoryLine() { return storyLine; }
@@ -49,6 +63,6 @@ public class    Book {
     }
     @Override
     public String toString() {
-        return title + ", " + isbn + ", " + published.toString();
+        return title + ", " + isbn + ", " + published.toString() + ", Grade: " + grade;
     }
 }
--- src/main/java/se/kth/databas/model/BooksDbMockImpl.java
+++ src/main/java/se/kth/databas/model/BooksDbMockImpl.java
@@ -84,8 +84,14 @@ public class BooksDbMockImpl implements BooksDbInterface {
                         int bookId = rs.getInt("bookId");
                         String isbn = rs.getString("ISBN");
                         String title = rs.getString("title");
-                        Date published = rs.getDate("publishDate");
-                        Book book = new Book(bookId, isbn, title, published);
+                        Date published = rs.getDate("published");
+
+                        // Fetch authors associated with the book
+                        List<Author> bookAuthors = getAuthorsForBook(bookId);
+
+                        Book book = new Book(isbn, title, published, bookId);
+                        book.getAuthors().addAll(bookAuthors);
+
                         result.add(book);
                     }
                 }
@@ -95,6 +101,26 @@ public class BooksDbMockImpl implements BooksDbInterface {
         }
 
         return result;
+    }
+
+    // A method to retrieve authors for a given book
+    private List<Author> getAuthorsForBook(int bookId) throws SQLException {
+        String sql = "SELECT a.* FROM Author a JOIN Book_Author ba ON a.authorId = ba.authorId WHERE ba.bookId = ?";
+        List<Author> authors = new ArrayList<>();
+
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setInt(1, bookId);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    int authorId = rs.getInt("authorId");
+                    String authorName = rs.getString("name");
+                    Author author = new Author(authorId, authorName);
+                    authors.add(author);
+                }
+            }
+        }
+
+        return authors;
     }
 
     public List<Author> searchAuthorsByName(String searchName) throws BooksDbException {
@@ -167,14 +193,14 @@ public class BooksDbMockImpl implements BooksDbInterface {
 
 
     private static final Book[] DATA = {
-            new Book(1, "123456789", "Databases Illuminated", new Date(2018, 1, 1)),
-            new Book(2, "234567891", "Dark Databases", new Date(1990, 1, 1)),
-            new Book(3, "456789012", "The buried giant", new Date(2000, 1, 1)),
-            new Book(4, "567890123", "Never let me go", new Date(2000, 1, 1)),
-            new Book(5, "678901234", "The remains of the day", new Date(2000, 1, 1)),
-            new Book(6, "234567890", "Alias Grace", new Date(2000, 1, 1)),
-            new Book(7, "345678911", "The handmaids tale", new Date(2010, 1, 1)),
-            new Book(8, "345678901", "Shuggie Bain", new Date(2020, 1, 1)),
-            new Book(9, "345678912", "Microserfs", new Date(2000, 1, 1)),
+            new Book(1, "123456789", "Databases Illuminated", new Date(2018, 1, 1),2),
+            new Book(2, "234567891", "Dark Databases", new Date(1990, 1, 1),1),
+            new Book(3, "456789012", "The buried giant", new Date(2000, 1, 1),2),
+            new Book(4, "567890123", "Never let me go", new Date(2000, 1, 1),3),
+            new Book(5, "678901234", "The remains of the day", new Date(2000, 1, 1),2),
+            new Book(6, "234567890", "Alias Grace", new Date(2000, 1, 1),3),
+            new Book(7, "345678911", "The handmaids tale", new Date(2010, 1, 1),3),
+            new Book(8, "345678901", "Shuggie Bain", new Date(2020, 1, 1),2),
+            new Book(9, "345678912", "Microserfs", new Date(2000, 1, 1),4),
     };
 }
