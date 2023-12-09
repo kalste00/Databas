22a0549d96ca051eecd67b89b43864a84eb2d867
Conrad <connevonne@gmail.com>
2023-12-09 18:07:29 +0100

    la till javadoc

--- src/main/java/se/kth/databas/model/BooksDbInterface.java
+++ src/main/java/se/kth/databas/model/BooksDbInterface.java
@@ -23,20 +23,62 @@ public interface BooksDbInterface {
      * @return true on successful connection.
      */
     public boolean connect(String database) throws BooksDbException;
-    
+    /**
+     * Disconnects from the database.
+     *
+     * @throws BooksDbException If an error occurs during disconnection.
+     */
     public void disconnect() throws BooksDbException;
-    
+    /**
+     * Searches for books by title in the database.
+     *
+     * @param title The title to search for.
+     * @return A list of books matching the title.
+     * @throws BooksDbException If an error occurs during the database query.
+     */
     public List<Book> searchBooksByTitle(String title) throws BooksDbException;
+    /**
+     * Searches for books by ISBN in the database.
+     *
+     * @param isbn The ISBN to search for.
+     * @return A list of books matching the ISBN.
+     * @throws BooksDbException If an error occurs during the database query.
+     */
     List<Book> searchBooksByISBN(String isbn) throws BooksDbException;
-
+    /**
+     * Searches for books by author in the database.
+     *
+     * @param author The author's name to search for.
+     * @return A list of books written by the specified author.
+     * @throws BooksDbException If an error occurs during the database query.
+     */
     List<Book> searchBooksByAuthor(String author) throws BooksDbException;
-
+    /**
+     * Retrieves all books from the database.
+     *
+     * @return A list of all books in the database.
+     * @throws BooksDbException If an error occurs during the database query.
+     */
     List<Book> getAllBooks() throws BooksDbException;
 
+    /**
+     *
+     * adds a new book and trows if exeption if not.
+     * @throws BooksDbException
+     */
     void addBook(Book newItem) throws BooksDbException;
 
+    /**
+     *updates book with new info and if wrong throws an execption.
+     * @throws BooksDbException
+     */
     void updateBook(Book updatedItem) throws BooksDbException;
 
+    /**
+     *
+     * deletes book from list.
+     * @throws BooksDbException
+     */
     void deleteBook(Book itemToDelete) throws BooksDbException;
 
 
